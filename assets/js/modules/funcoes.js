15b607e74b1be49cdb7908bb03fd646625a8e638
Gabriel <105074547+gabrieszin@users.noreply.github.com>
2024-02-13 11:33:51 -0300

    Atualizações de CSS e JS para o componente de carregamento e conteúdo

--- assets/js/modules/funcoes.js
+++ assets/js/modules/funcoes.js
@@ -2,6 +2,174 @@ const action = () => {
   console.log('Ação...');
 };
 
+const createLinkGoogleCalendar = (data) => {
+  const calendarProps = {
+    initDate: data['subscription-start'] || moment().format('YYYYMMDD'),
+    finishDate: data['subscription-end'] || moment().format('YYYYMMDD'),
+    text: `Inscrição para Vestibular da ${data.name} - ${data['short-name']}`,
+    details: `Vestibular da ${data.name}`,
+    // TODO - Implementar localização da faculdade via CEP (?)
+    location: 'Belo Horizonte - MG',
+  };
+
+  return encodeURI(`https://calendar.google.com/calendar/render?action=TEMPLATE&dates=${calendarProps.initDate}/${calendarProps.finishDate}&details=${calendarProps.details}&location=${calendarProps.location}&text=${calendarProps.text}`);
+};
+
+const searchElementExibition = (event) => {
+  let searched = event.target.value.trim().toLowerCase();
+  const area = document.querySelector('.results-search-element-exibition');
+
+  if (area !== undefined) {
+    return null;
+  }
+
+  const universities = [
+    'Universidade Federal de Tocantins',
+    'Universidade Federal de Alagoas',
+    'Universidade Federal da Bahia',
+    'Universidade Federal de Campina Grande',
+    'Universidade Federal do Ceará',
+    'Universidade Federal do Espírito Santo',
+    'Universidade Federal de Goiás',
+    'Universidade Federal Fluminense',
+    'Universidade Federal de Juiz de Fora',
+    'Universidade Federal de Minas Gerais',
+    'Universidade Federal do Pará',
+    'Universidade Federal da Paraíba',
+    'Universidade Federal do Paraná',
+    'Universidade Federal de Pernambuco',
+    'Universidade Federal do Rio Grande do Norte',
+    'Universidade Federal do Rio Grande do Sul',
+    'Universidade Federal do Rio de Janeiro',
+    'Universidade Federal de Santa Catarina',
+    'Universidade Federal de Santa Maria',
+    'Universidade Federal Rural de Pernambuco',
+    'Universidade Federal Rural do Rio de Janeiro',
+    'Universidade Federal de Roraima',
+    'Universidade Federal de São Paulo',
+    'Universidade Federal de Lavras',
+    'Universidade Federal de Rondônia',
+    'Universidade do Rio de Janeiro',
+    'Universidade Amazonas',
+    'Universidade Brasília',
+    'Universidade Maranhão',
+    'Universidade Rio Grande',
+    'Universidade Federal de Uberlândia',
+    'Universidade Federal Acre',
+    'Universidade Federal Mato Grosso',
+    'Universidade Federal Ouro Preto',
+    'Universidade Federal Pelotas',
+    'Universidade Federal Piauí',
+    'Universidade Federal São Carlos',
+    'Universidade Federal Sergipe',
+    'Universidade Federal Viçosa',
+    'Universidade Federal Mato Grosso do Sul',
+    'Universidade Federal Amapá',
+  ];
+
+  const replaceAccents = [
+    {
+      org: 'ç',
+      rep: 'c',
+    },
+    {
+      org: 'á',
+      rep: 'a',
+    },
+    {
+      org: 'à',
+      rep: 'a',
+    },
+    {
+      org: 'ã',
+      rep: 'a',
+    },
+    {
+      org: 'â',
+      rep: 'a',
+    },
+    {
+      org: 'é',
+      rep: 'e',
+    },
+    {
+      org: 'è',
+      rep: 'e',
+    },
+    {
+      org: 'ê',
+      rep: 'e',
+    },
+    {
+      org: 'í',
+      rep: 'i',
+    },
+    {
+      org: 'ó',
+      rep: 'o',
+    },
+    {
+      org: 'ô',
+      rep: 'o',
+    },
+    {
+      org: 'ú',
+      rep: 'u',
+    },
+  ];
+
+  const replaceAccentsOfWord = (word) => {
+    let wordSanitezed = word;
+    replaceAccents.forEach((replace) => wordSanitezed = wordSanitezed.replaceAll(replace.org, replace.rep));
+    return wordSanitezed;
+  };
+
+  if (searched && searched.length > 0) {
+    searched = replaceAccentsOfWord(searched);
+
+    const consults = universities.filter((university) => replaceAccentsOfWord(university).toLowerCase().substring(0, searched.length) === searched || replaceAccentsOfWord(university).toLowerCase().split(' ').includes(searched) || replaceAccentsOfWord(university).toLowerCase().includes(searched));
+
+    if (consults && consults.length > 0) {
+      $(area).find('.list-group').html('');
+      // Show
+      consults.toSorted((a, b) => a.localeCompare(b)).toSpliced(3).forEach((consult) => {
+        $(area).find('.list-group').append(`<a href="#" class="list-group-item list-group-item-action">${consult}</a>`);
+        $(area).show(300);
+      });
+    } else {
+      // Hide
+      $(area).hide(300);
+      $(area).find('.list-group').html('');
+    }
+  } else {
+    // Hide
+    $(area).hide(300);
+    $(area).find('.list-group').html('');
+  }
+};
+
+const searchVestibulares = () => {
+  // Para pesquisar por tipo e local
+  const value = 'tipo:publica local: Belo Horizonte';
+  const valueHasLocal = value.match(/local:\s*[\wÀ-ú\s]+/gi);
+  const valueHasTipo = value.match(/tipo:\s*(p[u|ú]blica|particular)+/gi);
+
+  if (valueHasLocal) {
+    let sanitized = valueHasLocal[0];
+    if (sanitized.match(/tipo/gi)) sanitized = sanitized.replace(/tipo/gi, '').trim();
+    // console.log(sanitized.replace(/local:/gi, '').trim());
+  }
+
+  if (valueHasTipo) {
+    let sanitized = valueHasTipo[0];
+    if (sanitized.match(/local/gi)) sanitized = sanitized.replace(/local/gi, '').trim();
+    // console.log(sanitized.replace(/tipo/gi, '').trim());
+  }
+};
+
 export default {
   action,
+  createLinkGoogleCalendar,
+  searchElementExibition,
+  searchVestibulares,
 };
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,24 @@
 import exibirDadosProjeto from './modules/sobre.js';
 import {
-  criarTooltips, popovers, range, tooltips, 
+  atualizarDatas,
+  criarTooltips, popovers, range, tooltips,
 } from './modules/utilitarios.js';
 import dados from './modules/dados.js';
 import fns from './modules/funcoes.js';
-
-import { card } from './modules/content.js';
+import { card, card_default } from './modules/content.js';
 
 (() => {
-  // hljs.highlightAll();
-  
-  // Confirmar saída da página
-  window.onbeforeunload = (evento) => {
-    // Tem coisa pra salvar
-    if (Array.from($('input, textarea')).filter((e) => e.value.trim().length > 0).length > 0) {
-      // evento.preventDefault();
-    } else {
-      // Não tem coisa pra salvar
-    }
-  };
-  
+  let database;
+
   document.querySelectorAll('[data-recarrega-pagina]').forEach((botao) => {
     botao.addEventListener('click', () => {
       window.location.reload();
     });
   });
-  
+
   function atribuirLinks() {
     const linkElementos = document.querySelectorAll('[data-link]');
-    
+
     linkElementos.forEach((link) => {
       if (link.dataset.link && dados.links.find((e) => e.id === link.dataset.link)) {
         link.setAttribute('href', dados.links.find((e) => e.id === link.dataset.link).link);
@@ -36,223 +26,149 @@ import { card } from './modules/content.js';
       } else {
         console.warn('O link não foi implementado.');
       }
-      
+
       link.setAttribute('rel', 'noopener noreferrer');
     });
   }
-  
-  function atribuirAcoes() {
-    const elementos = document.querySelectorAll('[data-action]');
-    
+
+  function atribuirAcoes(e) {
     const acoes = {
       acao: fns.action,
+      searchElementExibition: fns.searchElementExibition,
     };
-    
-    elementos.forEach((elemento) => {
-      switch (elemento.dataset.action) {
-        case 'acao':
-        $(elemento).on('click', () => {
-          acoes.acao();
-        });
-        break;
-        
-        case 'contribute':
-        break;
-        
-        case 'active-search-vestibulares':
-        $(elemento).on('click', (event) => {
-          event.preventDefault();
-          const modal = $('#modal-search');
-          $(modal).modal('show');
-          setTimeout(() => {
-            $(modal).find('input[type=search]').focus();
-          }, 500);
-        });
-        break;
-        
-        case 'search-element-exibition':
-        $(elemento).on('input', (event) => {
-          let searched = event.target.value.trim().toLowerCase();
-          const area = $('.results-search-element-exibition');
-          
-          const universities = [
-            'Universidade Federal de Tocantins',
-            'Universidade Federal de Alagoas',
-            'Universidade Federal da Bahia',
-            'Universidade Federal de Campina Grande',
-            'Universidade Federal do Ceará',
-            'Universidade Federal do Espírito Santo',
-            'Universidade Federal de Goiás',
-            'Universidade Federal Fluminense',
-            'Universidade Federal de Juiz de Fora',
-            'Universidade Federal de Minas Gerais',
-            'Universidade Federal do Pará',
-            'Universidade Federal da Paraíba',
-            'Universidade Federal do Paraná',
-            'Universidade Federal de Pernambuco',
-            'Universidade Federal do Rio Grande do Norte',
-            'Universidade Federal do Rio Grande do Sul',
-            'Universidade Federal do Rio de Janeiro',
-            'Universidade Federal de Santa Catarina',
-            'Universidade Federal de Santa Maria',
-            'Universidade Federal Rural de Pernambuco',
-            'Universidade Federal Rural do Rio de Janeiro',
-            'Universidade Federal de Roraima',
-            'Universidade Federal de São Paulo',
-            'Universidade Federal de Lavras',
-            'Universidade Federal de Rondônia',
-            'Universidade do Rio de Janeiro',
-            'Universidade Amazonas',
-            'Universidade Brasília',
-            'Universidade Maranhão',
-            'Universidade Rio Grande',
-            'Universidade Federal de Uberlândia',
-            'Universidade Federal Acre',
-            'Universidade Federal Mato Grosso',
-            'Universidade Federal Ouro Preto',
-            'Universidade Federal Pelotas',
-            'Universidade Federal Piauí',
-            'Universidade Federal São Carlos',
-            'Universidade Federal Sergipe',
-            'Universidade Federal Viçosa',
-            'Universidade Federal Mato Grosso do Sul',
-            'Universidade Federal Amapá',
-          ];
-          
-          const replaceAccents = [
-            {
-              org: 'ç',
-              rep: 'c',
-            }, 
-            {
-              org: 'á',
-              rep: 'a',
-            },
-            {
-              org: 'à',
-              rep: 'a',
-            }, 
-            {
-              org: 'ã',
-              rep: 'a',
-            },
-            {
-              org: 'â',
-              rep: 'a',
-            },
-            {
-              org: 'é',
-              rep: 'e',
-            },
-            {
-              org: 'è',
-              rep: 'e',
-            },
-            {
-              org: 'ê',
-              rep: 'e',
-            },
-            {
-              org: 'í',
-              rep: 'i',
-            },
-            {
-              org: 'ó',
-              rep: 'o',
-            },
-            {
-              org: 'ô',
-              rep: 'o',
-            },
-            {
-              org: 'ú',
-              rep: 'u',
-            },
-          ];
-          
-          const replaceAccentsOfWord = (word) => {
-            let wordSanitezed = word;
-            replaceAccents.forEach((replace) => wordSanitezed = wordSanitezed.replaceAll(replace.org, replace.rep));
-            return wordSanitezed;
-          };
-          
-          if (searched && searched.length > 0) {
-            searched = replaceAccentsOfWord(searched);
-            
-            const consults = universities.filter((university) => replaceAccentsOfWord(university).toLowerCase().substring(0, searched.length) === searched || replaceAccentsOfWord(university).toLowerCase().split(' ').includes(searched) || replaceAccentsOfWord(university).toLowerCase().includes(searched));
-            
-            if (consults && consults.length > 0) {
-              $(area).find('.list-group').html('');
-              // Show
-              consults.toSorted((a, b) => a.localeCompare(b)).toSpliced(3).forEach((consult) => {
-                $(area).find('.list-group').append(`<a href="#" class="list-group-item list-group-item-action">${consult}</a>`);
-                $(area).show(300);
-              });
-            } else {
-              // Hide
-              $(area).hide(300);
-              $(area).find('.list-group').html('');
-            }
-          } else {
-            // Hide
-            $(area).hide(300);
-            $(area).find('.list-group').html('');
-          }
-        });
-        break;
-        
-        default:
-        console.warn('A ação não foi implementada.');
-        break;
+
+    const attribute = (elemento) => {
+      if (elemento.dataset.action) {
+        switch (elemento.dataset.action) {
+          case 'acao':
+          $(elemento).on('click', () => {
+            acoes.acao();
+          });
+          break;
+
+          case 'contribute':
+          break;
+
+          case 'active-search-vestibulares':
+          $(elemento).on('click', (event) => {
+            event.preventDefault();
+            const modal = $('#modal-search');
+            $(modal).modal('show');
+            setTimeout(() => {
+              $(modal).find('input[type=search]').focus();
+            }, 500);
+          });
+          break;
+
+          case 'search-element-exibition':
+          $(elemento).on('input', (event) => {
+            acoes.searchElementExibition(event);
+          });
+          break;
+
+          case 'report-error':
+          break;
+
+          default:
+          console.log(elemento.dataset.action);
+          console.warn('A ação não foi implementada.');
+          break;
+        }
       }
-    });
+    };
+
+    if (e) {
+      attribute(e);
+    } else {
+      const elementos = document.querySelectorAll('[data-action]');
+      elementos.forEach((elemento) => {
+        attribute(elemento);
+      });
+    }
   }
-  
+
   window.addEventListener('load', () => {
     $('.overlay').hide();
     exibirDadosProjeto();
+  });
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const mode = 0; // 0 - Desenvolvimento, 1 - Teste, 3 - Produção
+    tooltips();
+    popovers();
     atribuirLinks();
     atribuirAcoes();
     criarTooltips();
-  });
-  
-  window.addEventListener('DOMContentLoaded', () => {
-    tooltips();
-    popovers();
-    
+    atualizarDatas();
+
+    const loadFuncionsPage = () => {
+      criarTooltips();
+      atualizarDatas();
+    };
+
     // Teste
-    const replicateCount = 9;
-    
-    range({ max: replicateCount }).forEach(() => {
-      $('.cards').append(card.trim());
-    });
+    const carregarDadosDeTeste = () => {
+      const replicateCount = 9;
 
-    $('#search-vestibulares').on('input', (event) => {
-      const value = $(event.target).val().trim();
+      range({ max: replicateCount }).forEach(() => {
+        $('.cards').append(card_default.trim());
+      });
 
-      if (value.length === 0) {
-        return null;
-      }
-    });
+      $('#search-vestibulares').on('input', (event) => {
+        const value = $(event.target).val().trim();
+
+        if (value.length === 0) {
+          return null;
+        }
+      });
+    };
+
+    if (mode === 1) {
+      carregarDadosDeTeste();
+      loadFuncionsPage();
+    } else {
+      fetch('/assets/js/modules/database.json')
+      .then((response) => response.json())
+      .then(({ vestibules }) => {
+        database = vestibules;
+        vestibules.toSorted((a, b) => a.name.localeCompare(b.name)).forEach((exam) => {
+          $('.cards').append(card(exam));
+        });
+      })
+      .catch((error) => console.error(error))
+      .finally(() => {
+        loadFuncionsPage();
+      });
+    }
   });
-  
-  setTimeout(() => {
-    // $('#modal-confirm-redirect').modal('show');
-  }, 500);
+
+  window.subscribeInVestibular = (btn) => {
+    const modal = $('#modal-confirm-redirect');
+    const btnId = btn.dataset.id;
+    const cardId = btn.closest('.card').dataset.id;
+
+    if (btn && modal.length === 1 && (btnId === cardId) && ![btnId, cardId].includes(undefined)) {
+      const itemDatabase = database[btnId - 1];
+      if (itemDatabase) {
+        // TODO - Implementar orientação para click nos botões do modal
+        $(modal).modal('show');
+      } else {
+        //
+      }
+    }
+  };
+
+  window.createAlarmSubscription = (btn) => {
+    const btnId = btn.dataset.id;
+    const cardId = btn.closest('.card').dataset.id;
+
+    if (btn && (btnId === cardId) && ![btnId, cardId].includes(undefined)) {
+      const itemDatabase = database[btnId - 1];
+
+      if (itemDatabase) {
+        const link = fns.createLinkGoogleCalendar(itemDatabase);
+        if (link) window.open(link, '_blank', 'noopener noreferrer');
+      }
+    }
+  };
 })();
-
-const value = 'tipo:publica local: Belo Horizonte';
-const valueHasLocal = value.match(/local:\s*[\wÀ-ú\s]+/gi);
-const valueHasTipo = value.match(/tipo:\s*(p[u|ú]blica|particular)+/gi);
-
-if (valueHasLocal) {
-  let sanitized = valueHasLocal[0];
-  if (sanitized.match(/tipo/gi)) sanitized = sanitized.replace(/tipo/gi, '').trim(); 
-  console.log(sanitized.replace(/local:/gi, '').trim());
-}
-
-if (valueHasTipo) {
-  let sanitized = valueHasTipo[0];
-  if (sanitized.match(/local/gi)) sanitized = sanitized.replace(/local/gi, '').trim(); 
-  console.log(sanitized.replace(/tipo/gi, '').trim());
-}
