591bee9b6b3e8be9d110875449b15a3b0883dc52
Gabriel <105074547+gabrieszin@users.noreply.github.com>
2024-03-16 19:21:46 -0300

    Adicionadas novas funcionalidades ao projeto de Vestibulares

--- assets/js/modules/sobre.js
+++ assets/js/modules/sobre.js
@@ -3,7 +3,7 @@ const apresentarDadosProjeto = (dados_do_projeto, novas_funcionalidades) => {
   console.groupCollapsed(`${dados_do_projeto['Project name'] ?? 'Projeto'}, Version ${dados_do_projeto.Version ?? '-'}`);
   console.table(dados_do_projeto);
   console.groupEnd();
-  
+
   console.groupCollapsed('New features');
   novas_funcionalidades.toSorted((a, b) => a.localeCompare(b)).forEach((feature) => { console.info(`${feature}`); });
   console.groupEnd();
@@ -18,11 +18,11 @@ let dados_do_projeto = {
 };
 
 const novas_funcionalidades = [
-  'Reformulação do layout da página de desligamento.',
-  'Adição de novo cálculo e geração de link para FID',
-  'Melhoria na navegação com o teclado.',
-  'Reduzindo os campos no formulário de proponentes.',
-  'Corrigidos erros na criação do arquivo de acompanhamento',
+  '# Início do projeto!',
+  'Adicionado busca de vestibulares por tipo ou localidade',
+  'Adicionamos busca por nome do vestibular',
+  'Implementado ferramenta de inscrição (apenas teste)',
+  'Incluimos função para criar link de lembrete no Google Calendar',
 ];
 
 // Carregando dados do arquivo de manifest.json
@@ -32,16 +32,16 @@ export default async () => {
   .then((response) => response.json())
   .then((manifest) => {
     const dados_manifest = {
-      'Project name': manifest.name, 
-      Developer: manifest.developer, 
-      Version: manifest.version, 
+      'Project name': manifest.name,
+      Developer: manifest.developer,
+      Version: manifest.version,
       'Release Date': manifest.release_date,
     };
     dados_do_projeto = ({ ...dados_manifest, ...dados_do_projeto });
-    
+
     Object.freeze(novas_funcionalidades);
     Object.freeze(dados_do_projeto);
-    
+
     apresentarDadosProjeto(dados_do_projeto, novas_funcionalidades);
     return true;
   })
